466fd163c3c9dca7999e15ae25ce1f8598213f73
Jessey White-Cinis <jcinis@gmail.com>
2017-03-21 22:20:56 -0400

    Adding config

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports {
+  "aws_access_key_id":"",
+  "aws_secret_access_key":"",
+  "aws_s3_bucket":"809f8c7e-cc23-484a-a3b0-e028084e4e4e",
+  "image_frequency":60, // in seconds
+  "image_width":3280, // 1280 or 3280
+  "image_height":2464 // 1024 or 2464
+};
