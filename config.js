56235c31f7981aab492232374b903994de110742
Jessey White-Cinis <jcinis@gmail.com>
2017-03-27 23:46:34 -0400

    fixing config path

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
+const path = require('path');
+
 module.exports = {
   "aws_access_key_id":"",
   "aws_secret_access_key":"",
   "aws_s3_bucket":"",
-  "capture_dir":"./capture",
+  "capture_dir": path.join(__dirname,'capture'),
   "capture_frequency":60, // in seconds
   "capture_width":1280,   // 1280 or 3280
   "capture_height":1024   // 1024 or 2464,
