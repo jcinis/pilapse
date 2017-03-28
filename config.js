bc3e725b9e903a4af1fad16172f650819c5cd8d6
Jessey White-Cinis <jcinis@gmail.com>
2017-03-27 22:46:02 -0400

    Cleaning config

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  "debug":false,
   "aws_access_key_id":"",
   "aws_secret_access_key":"",
-  "aws_s3_bucket":"809f8c7e-cc23-484a-a3b0-e028084e4e4e",
+  "aws_s3_bucket":"",
   "capture_dir":"./capture",
   "capture_frequency":60, // in seconds
   "capture_width":1280,   // 1280 or 3280
